2f6ed15b83f52a6235b3f53efb640460b3918a09
Adrian Gliźniewicz <adrian2402glizniewicz@gmail.com>
2024-04-22 09:02:45 +0200

    First system manager tests

--- ECS_Tests/TestSystemManager.cpp
+++ ECS_Tests/TestSystemManager.cpp
@@ -1,13 +1,29 @@
 #include "pch.h"
-#include "SystemManager.h"
+#include "SystemManager.cpp"
 #include "Systems.h"
 
 class TestSystemManager : public ::testing::Test
 {
+protected:
+	void RegisterComponents()
+	{
+		componentManager.RegisterComponent<Position>();
+		componentManager.RegisterComponent<Motion>();
+	}
 
+protected:
+	ComponentManager componentManager{};
 };
 
+TEST_F( TestSystemManager, RegisterSystem_WhenCompoenntsUnregistered )
+{
+	SystemManager systemManager{ componentManager };
+	EXPECT_THROW( { systemManager.RegisterSystem<Movement>(); }, ECS_Exception );
+}
 TEST_F( TestSystemManager, RegisterSystem )
 {
-
+	RegisterComponents();
+	SystemManager systemManager{ componentManager };
+	systemManager.RegisterSystem<Movement>();
 }
+
