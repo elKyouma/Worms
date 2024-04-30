ee71e831fb8efd64c085fc94febe551e228e67c0
Adrian Gliźniewicz <adrian2402glizniewicz@gmail.com>
2024-04-30 13:01:58 +0200

    Timer

--- Worms/src/Time.h
+++ Worms/src/Time.h
@@ -1,12 +1,35 @@
 #pragma once
+#include <SDL2/SDL.h>
 
 namespace Time
 {
-	static float deltaTime = 0.f;
+	static double deltaTime = 0.0;
 
 	class Timer
 	{
 	public:
+		Timer()
+		{
+			lastTime = SDL_GetTicks64();
+		}
+
+		double Reset()
+		{
+			uint64_t newTime = SDL_GetTicks64();
+			deltaTime = newTime - lastTime;
+			lastTime = newTime;
+			return deltaTime;
+		}
+
+		double Measure()
+		{
+			uint64_t newTime = SDL_GetTicks64();
+			deltaTime = newTime - lastTime;
+			return deltaTime;
+		}
+
 	private:
+		uint64_t lastTime = 0;
+		double deltaTime = 0.0;
 	};
 };
