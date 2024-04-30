33e334a23a3a14659c478e7fe9b451492771dd7f
Adrian Gliźniewicz <adrian2402glizniewicz@gmail.com>
2024-04-30 13:41:29 +0200

    Implement deltaTime

---
+++ Worms/src/Time.cpp
@@ -0,0 +1,26 @@
+#include "Time.h"
+
+namespace Time
+{
+	double deltaTime = 0.0;
+
+	Timer::Timer()
+	{
+		lastTime = SDL_GetTicks64();
+	}
+
+	double Timer::Reset()
+	{
+		uint64_t newTime = SDL_GetTicks64();
+		deltaTime = (newTime - lastTime) / 1000.0;
+		lastTime = newTime;
+		return deltaTime;
+	}
+
+	double Timer::Measure()
+	{
+		uint64_t newTime = SDL_GetTicks64();
+		deltaTime = (newTime - lastTime) / 1000.0;
+		return deltaTime;
+	}
+};
--- Worms/src/Time.h
+++ Worms/src/Time.h
@@ -3,30 +3,14 @@
 
 namespace Time
 {
-	static double deltaTime = 0.0;
+	extern double deltaTime;
 
 	class Timer
 	{
 	public:
-		Timer()
-		{
-			lastTime = SDL_GetTicks64();
-		}
-
-		double Reset()
-		{
-			uint64_t newTime = SDL_GetTicks64();
-			deltaTime = newTime - lastTime;
-			lastTime = newTime;
-			return deltaTime;
-		}
-
-		double Measure()
-		{
-			uint64_t newTime = SDL_GetTicks64();
-			deltaTime = newTime - lastTime;
-			return deltaTime;
-		}
+		Timer();
+		double Reset();
+		double Measure();
 
 	private:
 		uint64_t lastTime = 0;
