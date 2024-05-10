975ffb4521d349ecad6fe44e26ff02f2aaaef523
Dawid Hebda <dawhebda@gmail.com>
2024-05-10 21:42:43 +0200

    Camera method: ChangePosition added

--- Worms/src/Camera.cpp
+++ Worms/src/Camera.cpp
@@ -1,10 +1,17 @@
 #include "Camera.h"
 #include "Input.h"
 #include "Time.h"
+
 void Camera::Update()
 {
 	if ( !inputs_enabled ) return;
 
 	ChangeX( Input::Get().CameraHorizontal() * Time::deltaTime * CAMERA_SPEED );
 	ChangeY( Input::Get().CameraVertical() * Time::deltaTime * CAMERA_SPEED );
+}
+
+void Camera::ChangePosition( float x, float y )
+{
+	_x = x;
+	_y = y;
 }
