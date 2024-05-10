23a506ebd24af1abfa6e10907126ac4a207e5b99
Dawid Hebda <dawhebda@gmail.com>
2024-05-10 11:33:17 +0200

    added WormManager class with addTeam functionality

--- Worms/src/WormManager.cpp
+++ Worms/src/WormManager.cpp
@@ -1,1 +1,24 @@
 #include "WormManager.h"
+
+WormManager::WormManager( SDL_Renderer* renderer, World* world )
+	: _renderer(renderer), _world(world), _teams() {}
+
+void WormManager::addTeam( int size )
+{
+	WormTeam* new_team = new WormTeam;
+	for ( int i = 0; i < size; i++ ) {
+		new_team->addWorm( new Worm( _renderer, _world ));
+	}
+}
+
+WormManager::~WormManager() {
+	if ( !_teams.empty() )
+	{
+		for ( auto& team : _teams )
+		{
+			delete team;
+			team = nullptr;
+		}
+		_teams.clear();
+	}
+}
--- Worms/src/WormManager.h
+++ Worms/src/WormManager.h
@@ -1,2 +1,19 @@
 #pragma once
 #include "Worm.h"
+
+class WormManager
+{
+public:
+	WormManager( SDL_Renderer* renderer, World* world );
+	WormManager( const WormManager& ) = delete;
+	WormManager( WormManager&& ) = delete;
+
+	void addTeam(int size);
+
+	~WormManager();
+private:
+	SDL_Renderer* _renderer = NULL;
+	World* _world = NULL;
+	std::vector<WormTeam*> _teams;
+	WormTeam* _activeTeam = nullptr;
+};
