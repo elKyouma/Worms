90b8f35fa1d8f42fa77be5a1535351631b854a2b
Jakub Szydełko <kubarroo248@interia.pl>
2024-05-14 20:09:45 +0200

    b2ColliderDraw DrawSegment method added

--- Worms/src/b2ColliderDraw.cpp
+++ Worms/src/b2ColliderDraw.cpp
@@ -6,10 +6,17 @@ void b2ColliderDraw::DrawPolygon( const b2Vec2* vertices, int32 vertexCount, con
 	SDL_Point* points = new SDL_Point[vertexCount];
 	for ( int i = 0; i < vertexCount; i++ )
 	{
-		points[i].x = vertices[i].x;
-		points[i].y = vertices[i].y;
+		points[i].x = static_cast<int>(vertices[i].x);
+		points[i].y = static_cast<int>(vertices[i].y);
 	}
 	SDL_RenderDrawLines( renderer, points, vertexCount );
 
 	delete[] points;
+}
+
+void b2ColliderDraw::DrawSegment( const b2Vec2& p1, const b2Vec2& p2, const b2Color& color )
+{
+	SDL_Point point1( p1.x, p1.y );
+	SDL_Point point2( p2.x, p2.y );
+	SDL_RenderDrawLine( renderer, point1.x, point1.y, point2.x, point2.y );
 }
