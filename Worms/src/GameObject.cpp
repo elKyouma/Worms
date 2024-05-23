07b837b96d074ba18514e9d78b01e49d730a6301
Jakub Szydełko <kubarroo248@interia.pl>
2024-05-23 18:51:39 +0200

    GameObject class

---
+++ Worms/src/GameObject.cpp
@@ -0,0 +1,8 @@
+#include "GameObject.h"
+
+void GameObject::Initialise( SDL_Renderer* newRenderer, World* newWorld )
+{
+	renderer = newRenderer;
+	world = newWorld;
+	objectId = world->CreateEntity();
+}
---
+++ Worms/src/GameObject.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "ECS/ECS_Types.h"
+#include "ECS/World.h"
+#include "SDL2/SDL.h"
+
+class GameObject
+{
+public:
+	GameObject() : renderer(nullptr), world(nullptr) {};
+	void Initialise( SDL_Renderer* newRenderer, World* newWorld );
+	virtual void Update() {};
+	virtual void Render() {};
+	EntityId getId() { return objectId; }
+
+private:
+	EntityId objectId;
+	World* world;
+	SDL_Renderer* renderer;
+};
+
