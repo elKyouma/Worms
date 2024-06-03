07f74188419415aa3cd86a147e42d39da5ea5581
Dawid Hebda <dawhebda@gmail.com>
2024-06-03 07:44:31 +0200

    HealthBar now uses Follow component to follow Worm

--- Worms/src/Game/Player/HealthBar.cpp
+++ Worms/src/Game/Player/HealthBar.cpp
@@ -7,19 +7,11 @@ HealthBar::HealthBar( SDL_Renderer* renderer, World* world, EntityId newParentId
 {
 	Initialise( renderer, world );
 
-	parentId = newParentId;
-
 	position = &world->AddComponent<Position>( objectId, { 0, 0 } );
 	hp = &world->AddComponent<Health>( objectId, { health } );
+	target = &world->AddComponent<Follow>( objectId, { newParentId, 0.0, 0.3 } );
 
 	Sprite& spriteComponent = world->AddComponent<Sprite>( objectId );
 	spriteComponent.texture = IMG_LoadTexture( renderer, "heart.png" );
 	SDL_CHECK( spriteComponent.texture );
-}
-
-void HealthBar::Update()
-{
-	Position parentPosition = world->GetComponent<Position>( parentId );
-	position->x = parentPosition.x;
-	position->y = parentPosition.y + 0.3;
 }
